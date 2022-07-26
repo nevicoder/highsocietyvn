f0ff3c2470511bf25b16f58b4dc5aa687b697cc9
nevicoder <nevi@Nevi-T450s.localdomain>
2022-07-26 20:10:48 +0700

    add cors

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const postDashboardLogin = async (req, res, next) => {
   console.log("client connected");
   const username = await req.body.username;
